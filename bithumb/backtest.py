6ada95e865bd88d117e0ea487963895b8fa4e3d9
jun <chchdelm3@icloud.com>
2021-05-07 14:38:31 +0900

    backtesting

--- bithumb/backtest.py
+++ bithumb/backtest.py
@@ -8,7 +8,7 @@ def calMACD(df, m_NumFast=14, m_NumSlow=30, m_NumSignal=10):
     df['MACD'] = df['EMAFast'] - df['EMASlow']
     df['MACD_Signal'] = df['MACD'].ewm( span = m_NumSignal, min_periods = m_NumSignal - 1 ).mean()
     df['MACD_OSC'] = df['MACD'] - df['MACD_Signal']
-    return df['MACD_OSC'][-1]
+    return df['MACD_OSC']
 
 def calStochastic(df, n=14, m=7, t=7):
     ndays_high = df.high.rolling(window=n, min_periods=1).max()
@@ -18,16 +18,14 @@ def calStochastic(df, n=14, m=7, t=7):
     slow_d = slow_k.ewm(span=t).mean()
     slow_osc = slow_k - slow_d
     df = df.assign(fast_k=fast_k, fast_d=slow_k, slow_k=slow_k, slow_d=slow_d, slow_osc=slow_osc)
-    return df['slow_osc'][-1], df['slow_k'][-1]
+    return df['slow_osc'], df['slow_k']
 
 def cal_target(df):
-    yesterday = df.iloc[-2]
-    today = df.iloc[-1]
-    yesterday_range = yesterday['high'] - yesterday['low']
-    noise = 1 - abs(yesterday['open'] - yesterday['close']) / (yesterday['high'] - yesterday['low'])
-    target_bull = today['open'] + (yesterday_range * noise)
-    target_bear = today['open'] - (yesterday_range * noise)
-    return target_bull, target_bear
+    df['noise'] = 1 - abs(df['open'] - df['close']) / (df['high'] - df['low'])
+    df['range'] = (df['high'] - df['low']) * df['noise']
+    df['target_bull'] = df['open'] + df['range'].shift(1)
+    df['target_bear'] = df['open'] - df['range'].shift(1)
+    return df['target_bull'], df['target_bear']
 
 df = pybithumb.get_ohlcv("BTC")
 df['target_bull'] = cal_target(df)[0]
