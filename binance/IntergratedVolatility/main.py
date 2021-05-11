fe13a692a349d90a9eb2c6787185fab8b76b910d
hs <juullyy001@gmail.com>
2021-05-12 01:23:35 +0900

    create volatility strategy

--- binance/IntergratedVolatility/main.py
+++ binance/IntergratedVolatility/main.py
@@ -5,7 +5,7 @@ import time
 import telegram
 
 # telegram setting
-with open("heebot.txt") as f:
+with open("mombot.txt") as f:
     lines = f.readlines()
     my_token = lines[0].strip()
     chat_id = lines[1].strip()
@@ -50,12 +50,13 @@ for symbol in symbols:
     info[symbol]['ma'] = 0
 
 def calMACD(df, m_NumFast=12, m_NumSlow=26, m_NumSignal=9):
-    df['EMAFast'] = df['close'].ewm( span = m_NumFast, min_periods = m_NumFast - 1 ).mean()
-    df['EMASlow'] = df['close'].ewm( span = m_NumSlow, min_periods = m_NumSlow - 1 ).mean()
-    df['MACD'] = df['EMAFast'] - df['EMASlow']
-    df['MACD_Signal'] = df['MACD'].ewm( span = m_NumSignal, min_periods = m_NumSignal - 1 ).mean()
-    df['MACD_OSC'] = df['MACD'] - df['MACD_Signal']
-    return df['MACD_OSC'][-1]
+    EMAFast = df.close.ewm( span = m_NumFast, min_periods = m_NumFast - 1 ).mean()
+    EMASlow = df.close.ewm( span = m_NumSlow, min_periods = m_NumSlow - 1 ).mean()
+    MACD = EMAFast - EMASlow
+    MACDSignal = MACD.ewm( span = m_NumSignal, min_periods = m_NumSignal - 1 ).mean()
+    macd_osc = MACD - MACDSignal
+    df = df.assign(EMAFast=EMAFast, EMASlow=EMASlow, MACD=MACD, MACDSignal=MACDSignal, macd_osc=macd_osc)
+    return df['macd_osc'][-1]
 
 def calStochastic(df, n=9, m=5, t=3):
     ndays_high = df.high.rolling(window=n, min_periods=1).max()
@@ -95,6 +96,11 @@ def save_info():
         info[symbol]['target_bear'] = cal_target(df)[1]
         info[symbol]['ma'] = calMA(df)
         info[symbol]['open_price'] = binance.fetch_ticker(symbol=symbol)['open'] # 현재가 조회
+
+        print(f"코인: {symbol}\n현재가: {current_price}")
+        print(f"지정 매수가: {info[symbol]['target_bull']}\n지정 매도가: {info[symbol]['target_bear']}")
+        print(f"Stochastic OSC: {info[symbol]['slow_osc']}\nEMA: {info[symbol]['ma']}")
+        print(f"포지션 상태: {info[symbol]['position']}\n총 보유 코인: {total_hold}개\n")
         time.sleep(1)
     bot.sendMessage(chat_id = chat_id, text=f"오늘 코인 목표가 계산이 끝났습니다.")
 
@@ -115,13 +121,13 @@ def price_unit(price):
 
 # 투자금액 조정
 def adjust_money(free_balance, total_hold):
-    if total_hold < 5:
-        available_hold = 5 - total_hold
-        money = round((free_balance * 3 / available_hold - 6), -1)
+    if total_hold < 3:
+        available_hold = 3 - total_hold
+        money = round((free_balance * 2 / available_hold - 6), -1)
         return money
 
 total_hold = 0
-bot.sendMessage(chat_id = chat_id, text=f"Intergrated Volatility 전략 시작합니다. 화이팅!")
+bot.sendMessage(chat_id = chat_id, text=f"Volatility 전략 시작합니다. 화이팅!")
 save_info()
 
 while True:
@@ -158,11 +164,10 @@ while True:
                 free_balance = round(binance.fetch_balance()['USDT']['free'], 2)
                 money = adjust_money(free_balance=free_balance, total_hold=total_hold) # 코인별 투자금액
                 bot.sendMessage(chat_id = chat_id, text=f"시작잔고: {start_balance} -> 현재잔고: {total_balance}원\n오늘 1코인당 투자금액: {money}")
-                start_balance = total_balance
                 save_info()
 
             # 조건을 만족하면 지정가 매수
-            elif info[symbol]['position'] == 'wait' and total_hold < 3 and info[symbol]['slow_osc'] > 0 and info[symbol]['ma'] < info[symbol]['open_price'] and (info[symbol]['target_bull'] * 0.995) <= current_price <= (info[symbol]['target_bull'] * 1.005):
+            elif info[symbol]['position'] == 'wait' and total_hold < 3 and info[symbol]['slow_osc'] > 0 and info[symbol]['ma'] < info[symbol]['open_price'] and info[symbol]['macd_osc'] > 0 and (info[symbol]['target_bull'] * 0.995) <= current_price <= (info[symbol]['target_bull'] * 1.005):
                 amount = money / current_price # 매수할 코인 개수
                 binance.create_order(symbol=symbol, type="MARKET", side="buy", amount=amount)
                 info[symbol]['price'] = current_price
@@ -172,7 +177,7 @@ while True:
                 bot.sendMessage(chat_id = chat_id, text=f"{symbol} 롱 포지션\n매수가: {current_price}\n투자금액: {money}\n총 보유 코인: {total_hold}")
 
             # 조건을 만족하면 지정가 공매도
-            elif info[symbol]['position'] == 'wait' and total_hold < 3 and info[symbol]['slow_osc'] < 0 and info[symbol]['ma'] > info[symbol]['open_price'] and (info[symbol]['target_bear'] * 0.995) <= current_price <= (info[symbol]['target_bear'] * 1.005):
+            elif info[symbol]['position'] == 'wait' and total_hold < 3 and info[symbol]['slow_osc'] < 0 and info[symbol]['ma'] > info[symbol]['open_price'] and info[symbol]['macd_osc'] < 0 and (info[symbol]['target_bear'] * 0.995) <= current_price <= (info[symbol]['target_bear'] * 1.005):
                 amount = money / current_price # 매도할 코인 개수
                 binance.create_order(symbol=symbol, type="MARKET", side="sell", amount=amount)
                 info[symbol]['price'] = current_price
