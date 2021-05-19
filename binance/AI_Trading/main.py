56253666957846ae2ec665f8c2908e8d7ace7df6
jun <chchdelm3@icloud.com>
2021-05-19 09:47:29 +0900

    create ai strategy

--- binance/AI_Trading/main.py
+++ binance/AI_Trading/main.py
@@ -2,118 +2,135 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import pyupbit
 import datetime
 import time
 import telegram
+import ccxt
+
+# 거래소 설정
+# 파일로부터 apiKey, Secret 읽기
+with open("binance.txt") as f:
+    lines = f.readlines()
+    api_key = lines[0].strip()
+    secret = lines[1].strip()
+
+binance = ccxt.binance({
+    'apiKey': api_key,
+    'secret': secret,
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'future',
+    }
+})
 
 # telegram setting
 my_token = '1725701346:AAFoCMr7xeQwjaqvBsOPoIS99PyRFwVFK_E'
 bot = telegram.Bot(token = my_token)
 chat_id = '1459236537'
 
+tickers = ['LINK/USDT', 'ADA/USDT']
+
+info = {}
+for ticker in tickers:
+    info[ticker] = {}
+    info[ticker]['amount'] = 0 # 코인 매수/매도 갯수
+    info[ticker]['position'] = 'wait' # 현재 거래 포지션 (long / short / wait)
+    info[ticker]['price'] = 0 # 코인 거래한 가격
+    info[ticker]['trade_signal'] = None # 거래 신호 (buy or sell)
+    info[ticker]['linReg_confidence'] = 0
+
 def calPrice(ticker):
-    df = pyupbit.get_ohlcv(ticker=ticker, interval="minute15", count=200)
-# Create a variable for predicting 'n' days out into the future
+    ohlcv = binance.fetch_ohlcv(ticker, '1d')
+    df = pd.DataFrame(ohlcv, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
+    df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
+    df.set_index('datetime', inplace=True)
+
     projection = 1
-# Create a new column called projection
     df['Prediction'] = df[['close']].shift(-projection)
 
-# Create the independent data set (X)
     X = np.array(df[['close']])
-# Remove the last 14 rows
     X = X[:-projection]
 
-# Create the dependent data set (y)
     y = df['Prediction'].values
     y = y[:-projection]
 
-# Split the data into 85% training and 15% testing data sets
     x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=.15)
-
-# Create and train the model
     linReg = LinearRegression()
-# Train the model
     linReg.fit(x_train, y_train)
 
-# Test the model using score
     linReg_confidence = linReg.score(x_test, y_test)
+    info[ticker]['linReg_confidence'] = linReg_confidence
     print('Linear Regression Confidence:', linReg_confidence)
-
-# Create a variable called x_projection and set it equal to the last 14 rows of data from the original data set
     x_projection = np.array(df[['close']])[-projection:]
-
-# Print the linear regression models predictions for the next 14 days
     linReg_prediction = linReg.predict(x_projection)
     print(linReg_prediction)
-
-# Show the data set
     print(df)
+
     if linReg_prediction > df['close'][-1]:
         print("Buy")
-        trade = 1
+        info[ticker]['trade_signal'] = True
     elif linReg_prediction < df['close'][-1]:
         print("Sell")
-        trade = 0
-    return trade
+        info[ticker]['trade_signal'] = False
 
-# 원화 마켓 주문 가격 단위
-def price_unit(price):
-    if price < 10:
-        price = round(price, 2)
-    elif 10 <= price < 100:
-        price = round(price, 1)
-    elif 100 <= price < 1000:
-        price = round(price)
-    elif 1000 <= price < 100000:
-        price = round(price, -1)
-    elif 100000 <= price < 1000000:
-        price = round(price, -2)
-    elif price >= 1000000:
-        price = round(price, -3)
-    return price
+def savePrice(ticker):
+    while True:
+        calPrice(ticker)
+        if info[ticker]['linReg_confidence'] > 0.97:
+            break
 
-# 객체 생성
-f = open("upbit.txt")
-lines = f.readlines()
-access = lines[0].strip()
-secret = lines[1].strip()
-f.close()
-upbit = pyupbit.Upbit(access, secret)
-
-ticker = "KRW-BTC"
-trade = 0
-hold = False
-start_price = 0
-end_price = 0
-profit = 0
-linReg_prediction = 0
+bot.sendMessage(chat_id = chat_id, text=f"AI 전략 시작합니다. 화이팅!")
 
 while True:
     try:
         now = datetime.datetime.now()
-        if now.minute % 15 == 0:
+        if now.minute % 30 == 0:
+            money = round(binance.fetch_balance()['USDT']['free'] / 2, 0)
+            for ticker in tickers:
+                savePrice(ticker)
+                current_price = binance.fetch_ticker(symbol=ticker)['close'] # 현재가 조회
+                amount = money / current_price # 거래할 코인 갯수
+                if info[ticker]['position'] == 'wait' and info[ticker]['trade_signal'] == True:
+                    binance.create_market_buy_order(symbol=ticker, amount=amount) # 시장가 매수
+                    info[ticker]['price'] = current_price
+                    info[ticker]['position'] == 'long'
+                    info[ticker]['amount'] = amount # 코인 갯수 저장
+                    print(f"{ticker}를 매수했습니다!\n")
+                    bot.sendMessage(chat_id = chat_id, text=f"{ticker} 롱 포지션\n매수가: {current_price}\n투자금액: {money}")
 
-            while True:
-                calPrice(ticker)
-                if linReg_prediction > 0.98:
-                    break
+                elif info[ticker]['position'] == 'wait' and info[ticker]['trade_signal'] == False:
+                    binance.create_market_sell_order(symbol=ticker, amount=amount) # 시장가 매도
+                    info[ticker]['price'] = current_price
+                    info[ticker]['position'] == 'short'
+                    info[ticker]['amount'] = amount # 코인 갯수 저장
+                    print(f"{ticker}를 매도했습니다!\n")
+                    bot.sendMessage(chat_id = chat_id, text=f"{ticker} 숏 포지션\n매도가: {current_price}\n투자금액: {money}")
 
-            if hold == False and trade == True:
-                upbit.buy_market_order(ticker=ticker, price=50000)
-                start_price = pyupbit.get_current_price(ticker)  # 코인 현재가
-                hold = True
-                print(f"{ticker}를 매수했습니다!\n")
-                bot.sendMessage(chat_id = chat_id, text=f"코인: {ticker} 매수했습니다.")
+                elif info[ticker]['position'] == 'long' and info[ticker]['trade_signal'] == False:
+                    binance.create_order(symbol=ticker, type="MARKET", side="sell", amount=info[ticker]['amount'], params={"reduceOnly": True})
+                    profit = (current_price - info[ticker]['price']) / info[ticker]['price'] * 100 # 수익률 계산
+                    bot.sendMessage(chat_id = chat_id, text=f"코인: {ticker} (롱)\n매수가: {info[ticker]['price']} -> 매도가: {current_price}\n수익률: {profit:.2f}%")
+                    print(f"코인: {ticker} (롱) 포지션 청산\n매수가: {info[ticker]['price']} -> 매도가: {current_price}\n수익률: {profit}")
+                    time.sleep(1)
+                    binance.create_market_sell_order(symbol=ticker, amount=amount) # 시장가 매도
+                    info[ticker]['price'] = current_price
+                    info[ticker]['position'] == 'short'
+                    info[ticker]['amount'] = amount # 코인 갯수 저장
+                    print(f"{ticker}를 매도했습니다!\n")
+                    bot.sendMessage(chat_id = chat_id, text=f"{ticker} 숏 포지션\n매도가: {current_price}\n투자금액: {money}")
 
-            elif hold == True and trade == False:
-                coin_balance = upbit.get_balance(ticker)  # 코인 잔고
-                upbit.sell_market_order(ticker=ticker, volume=coin_balance)
-                end_price = pyupbit.get_current_price(ticker)  # 코인 현재가
-                profit = (end_price - start_price) / start_price * 100
-                hold = False
-                print(f"{ticker}를 매도했습니다!\n수익률: {profit}\n\n")
-                bot.sendMessage(chat_id = chat_id, text=f"코인: {ticker} 매도했습니다.\n수익률: {profit}")
-            time.sleep(60)
+                elif info[ticker]['position'] == 'short' and info[ticker]['trade_signal'] == True:
+                    binance.create_order(symbol=ticker, type="MARKET", side="buy", amount=info[ticker]['amount'], params={"reduceOnly": True})
+                    profit = (info[ticker]['price'] - current_price) / current_price * 100 # 수익률 계산
+                    bot.sendMessage(chat_id = chat_id, text=f"코인: {ticker} (숏)\n매도가: {info[ticker]['price']} -> 매수가: {current_price}\n수익률: {profit:.2f}%")
+                    print(f"코인: {ticker} (숏) 포지션 청산\n매도가: {info[ticker]['price']} -> 매수가: {current_price}\n수익률: {profit}")
+                    time.sleep(1)
+                    binance.create_market_buy_order(symbol=ticker, amount=amount) # 시장가 매수
+                    info[ticker]['price'] = current_price
+                    info[ticker]['position'] == 'long'
+                    info[ticker]['amount'] = amount # 코인 갯수 저장
+                    print(f"{ticker}를 매수했습니다!\n")
+                    bot.sendMessage(chat_id = chat_id, text=f"{ticker} 롱 포지션\n매수가: {current_price}\n투자금액: {money}")
+        time.sleep(30)
     except Exception as e:
         bot.sendMessage(chat_id = chat_id, text=f"Error!!\n{e}")
