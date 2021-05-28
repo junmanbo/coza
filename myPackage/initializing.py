742cf42a5a805e60f3a0affc0f2b3160de1aad1e
jun <chchdelm3@icloud.com>
2021-05-28 17:49:26 +0900

    update

--- myPackage/initializing.py
+++ myPackage/initializing.py
@@ -9,6 +9,7 @@ binance = ccxt.binance({
 })
 
 tickers = binance.load_markets().keys()
+#  tickers = ('BTC/USDT', 'ETH/USDT')
 
 symbols = list(tickers)
 # 코인별 저장 정보값 초기화
@@ -18,6 +19,7 @@ for symbol in symbols:
     info[symbol]['amount'] = 0 # 코인 매수/매도 갯수
     info[symbol]['position'] = 'wait' # 현재 거래 포지션 (long / short / wait)
     info[symbol]['price'] = 0 # 코인 거래한 가격
+    #  info[symbol]['total_trading'] = 0 # 코인 거래한 가격
 
 with open('./Data/binance_short.txt', 'w') as f:
     f.write(json.dumps(info)) # use `json.loads` to do the reverse
