d3d83cf6f9d8d3e9d3bc25feff6f38c21a9e0c77
jun <chchdelm3@icloud.com>
2021-05-29 23:15:35 +0900

    balance

---
+++ day_notification.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import ccxt
+import datetime
+import time
+import telegram
+import json
+import logging
+import pandas as pd
+from myPackage import indicators as indi
+
+# telegram 설정
+with open('./Api/mybot.txt') as f:
+    lines = f.readlines()
+    my_token = lines[0].strip()
+    chat_id = lines[1].strip()
+bot = telegram.Bot(token = my_token)
+
+# 거래소 설정
+with open('./Api/binance.txt') as f:
+    lines = f.readlines()
+    api_key = lines[0].strip()
+    secret = lines[1].strip()
+
+# 기본 옵션: 선물
+binance = ccxt.binance({
+    'apiKey': api_key,
+    'secret': secret,
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'future',
+    }
+})
+
+# 코인 정보 저장 파일 불러오기
+with open('./Data/balance.txt', 'r') as f:
+    data = f.read()
+    start_balance = json.loads(data)
+
+end_balance = binance.fetch_balance()['USDT']['total']
+
+bot.sendMessage(chat_id = chat_id, text=f"하루 정산\n어제: ${start_balance} -> 오늘: ${end_balance}\n차액: ${end_balance - start_balance}")
+
+# 파일에 수집한 정보 및 거래 정보 파일에 저장
+with open('./Data/balance.txt', 'w') as f:
+    f.write(json.dumps(end_balance))
