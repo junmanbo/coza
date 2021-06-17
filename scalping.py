75a67e6b390b4ef19ccea6b838040b637cfadd35
jun <chchdelm3@icloud.com>
2021-06-18 07:39:43 +0900

    update

--- scalping.py
+++ scalping.py
@@ -116,13 +116,15 @@ while True:
         try:
             current_price = binance.fetch_ticker(symbol)['close'] # 현재가 조회
 
-            df = getOHLCV(symbol, '15m')
+            df = getOHLCV(symbol, '30m')
             stoch_osc = indi.calStochastic(df, 9, 3, 3)[1]
             df = getOHLCV(symbol, '1m')
             stoch_osc_before, stoch_osc_now = indi.calStochastic(df, 9, 3, 3)
             mfi = indi.cal_mfi(df, 15)
+            vol_short = indi.cal_vol_ema(df, 5)
+            vol_long = indi.cal_vol_ema(df, 10)
 
-            logging.info(f'Stochastic: {stoch_osc} {stoch_osc_before} {stoch_osc_now} MFI: {mfi}')
+            logging.info(f'Stochastic: {stoch_osc} {stoch_osc_before} {stoch_osc_now} MFI: {mfi} Volume: {vol_short} {vol_long}')
 
             if info[symbol]['position'] == 'long':
                 # 1시간 단위로 코인 변경(이전 코인 청산)
@@ -191,7 +193,8 @@ while True:
                     logging.info(f"{symbol} (숏) + 진행중! 본절로스 갱신")
 
             # 조건 만족시 Long Position
-            elif info[symbol]['position'] == 'wait' and stoch_osc_before < 0 and stoch_osc_now > 0 and stoch_osc > 0 and mfi < 25 and now.minute < 50:
+            elif info[symbol]['position'] == 'wait' and stoch_osc_before < 0 and stoch_osc_now > 0 and stoch_osc > 0 and \
+                    vol_short > vol_long and mfi < 25 and now.minute < 50:
                 # 투자를 위한 세팅
                 bid_ask = binance.fetch_bids_asks(symbols=symbol)
                 current_price = bid_ask[symbol]['ask']
@@ -210,7 +213,8 @@ while True:
                 bot.sendMessage(chat_id=chat_id, text=f"{strategy} {symbol} (Long)\nAmount: ${amount:.2f}")
 
             # 조건 만족시 Short Position
-            elif info[symbol]['position'] == 'wait' and stoch_osc_before > 0 and stoch_osc_now < 0 and stoch_osc < 0 and mfi > 75 and now.minute < 50:
+            elif info[symbol]['position'] == 'wait' and stoch_osc_before > 0 and stoch_osc_now < 0 and stoch_osc < 0 and \
+                    vol_short > vol_long and mfi > 75 and now.minute < 50:
                 # 투자를 위한 세팅
                 bid_ask = binance.fetch_bids_asks(symbols=symbol)
                 current_price = bid_ask[symbol]['bid']
