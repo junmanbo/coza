be9b9bd937d0df18e9c8c54085899f41e012af95
jun <chchdelm3@icloud.com>
2021-06-24 23:09:36 +0900

    systemctl to crontab

--- myPackage/initializing.py
+++ myPackage/initializing.py
@@ -9,7 +9,6 @@ binance = ccxt.binance({
 })
 
 tickers = binance.load_markets().keys()
-#  tickers = ('BTC/USDT', 'ETH/USDT')
 
 symbols = list(tickers)
 # 코인별 저장 정보값 초기화
@@ -19,7 +18,6 @@ for symbol in symbols:
     info[symbol]['quantity'] = 0 # 코인 매수/매도 갯수
     info[symbol]['position'] = 'wait' # 현재 거래 포지션 (long / short / wait)
     info[symbol]['price'] = 0 # 코인 거래한 가격
-    info[symbol]['start_price'] = 0
 
 with open('./Data/binance_swing.txt', 'w') as f:
     f.write(json.dumps(info)) # use `json.loads` to do the reverse
--- swing.py
+++ swing.py
@@ -76,91 +76,84 @@ fee = 0.2 / 100
 logging.info(f"{strategy}\n현재보유: {current_hold}개\n투자할 코인: {total_hold-current_hold}개")
 bot.sendMessage(chat_id=chat_id, text=f"{strategy}\nHolding: {current_hold}")
 
-while True:
-    now = datetime.datetime.now()
-    time.sleep(1)
+# 1코인 1번당 투자 금액
+total_balance = binance.fetch_balance()['USDT']['total']
+amount = total_balance * leverage / total_hold
+logging.info('Daily checking')
+for symbol in symbols:
+    try:
+        current_price = binance.fetch_ticker(symbol)['close'] # 현재가 조회
 
-    if now.hour == 9 and now.minute == 0 and 0 <= now.second <= 5:
-        # 1코인 1번당 투자 금액
-        total_balance = binance.fetch_balance()['USDT']['total']
-        amount = total_balance * leverage / total_hold
-        logging.info('Daily checking')
-        for symbol in symbols:
-            try:
-                current_price = binance.fetch_ticker(symbol)['close'] # 현재가 조회
+        df = getOHLCV(symbol, '1d')
+        stoch_osc_yes, stoch_osc_to = indi.calStochastic(df, 12, 5, 5)
+        macd = indi.cal_macd(df, 7, 14, 5)
+        mfi = indi.cal_mfi(df, 10)
 
-                df = getOHLCV(symbol, '1d')
-                stoch_osc_yes, stoch_osc_to = indi.calStochastic(df, 12, 5, 5)
-                macd = indi.cal_macd(df, 7, 14, 5)
-                mfi = indi.cal_mfi(df, 10)
+        logging.info(f'코인: {symbol}\nStochastic: {stoch_osc_yes} {stoch_osc_to} MACD: {macd} MFI: {mfi}')
 
-                logging.info(f'코인: {symbol}\nStochastic: {stoch_osc_yes} {stoch_osc_to} MACD: {macd} MFI: {mfi}')
+        if info[symbol]['position'] == 'wait' and current_hold < total_hold:
+            if stoch_osc_yes < -1 and stoch_osc_to > 1 and macd > 0:
+                quantity = amount / current_price
+                order = binance.create_limit_buy_order(symbol, quantity, current_price) # 지정가 매수 주문
 
-                if info[symbol]['position'] == 'wait' and current_hold < total_hold:
-                    if stoch_osc_yes < -1 and stoch_osc_to > 1 and macd > 0:
-                        quantity = amount / current_price
-                        order = binance.create_limit_buy_order(symbol, quantity, current_price) # 지정가 매수 주문
+                # 매수가, 포지션 상태, 코인 매수 양 저장
+                info[symbol]['price'] = current_price
+                info[symbol]['position'] = 'long'
+                info[symbol]['quantity'] = quantity
+                current_hold += 1
+                logging.info(f"{symbol} (롱)\n투자금액: ${amount:.2f}\n현재보유: {current_hold}개\n주문: {order}")
+                bot.sendMessage(chat_id=chat_id, text=f"{strategy} {symbol} (Long)\nAmount: ${amount:.2f}\nHolding: {current_hold}")
 
-                        # 매수가, 포지션 상태, 코인 매수 양 저장
-                        info[symbol]['price'] = current_price
-                        info[symbol]['start_price'] = current_price
-                        info[symbol]['position'] = 'long'
-                        info[symbol]['quantity'] = quantity
-                        current_hold += 1
-                        logging.info(f"{symbol} (롱)\n투자금액: ${amount:.2f}\n현재보유: {current_hold}개\n주문: {order}")
-                        bot.sendMessage(chat_id=chat_id, text=f"{strategy} {symbol} (Long)\nAmount: ${amount:.2f}\nHolding: {current_hold}")
+            elif stoch_osc_yes > 1 and stoch_osc_to < -1 and macd < 0:
+                quantity = amount / current_price
+                order = binance.create_limit_sell_order(symbol, quantity, current_price) # 지정가 매도 주문
 
-                    elif stoch_osc_yes > 1 and stoch_osc_to < -1 and macd < 0:
-                        quantity = amount / current_price
-                        order = binance.create_limit_sell_order(symbol, quantity, current_price) # 지정가 매도 주문
+                # 매수가, 포지션 상태, 코인 매수 양 저장
+                info[symbol]['price'] = current_price
+                info[symbol]['position'] = 'short'
+                info[symbol]['quantity'] = quantity
+                current_hold += 1
+                logging.info(f"{symbol} (숏)\n투자금액: ${amount:.2f}\n현재보유: {current_hold}개\n주문: {order}")
+                bot.sendMessage(chat_id=chat_id, text=f"{strategy} {symbol} (Short)\nAmount: ${amount:.2f}\nHolding: {current_hold}")
 
-                        # 매수가, 포지션 상태, 코인 매수 양 저장
-                        info[symbol]['price'] = current_price
-                        info[symbol]['start_price'] = current_price
-                        info[symbol]['position'] = 'short'
-                        info[symbol]['quantity'] = quantity
-                        current_hold += 1
-                        logging.info(f"{symbol} (숏)\n투자금액: ${amount:.2f}\n현재보유: {current_hold}개\n주문: {order}")
-                        bot.sendMessage(chat_id=chat_id, text=f"{strategy} {symbol} (Short)\nAmount: ${amount:.2f}\nHolding: {current_hold}")
+        elif info[symbol]['position'] == 'long':
+            if stoch_osc_yes > 0 and stoch_osc_to < 0 and mfi > 70:
+                order = binance.create_limit_sell_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매도 주문
+                info[symbol]['position'] = 'wait'
+                current_hold -= 1
+                profit = (current_price - info[symbol]['price']) / info[symbol]['price'] * 100
+                logging.info(f"{symbol} (롱) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
+                bot.sendMessage(chat_id = chat_id, text=f"{symbol} (롱) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
+            elif macd < 0:
+                order = binance.create_limit_sell_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매도 주문
+                info[symbol]['position'] = 'wait'
+                current_hold -= 1
+                profit = (current_price - info[symbol]['price']) / info[symbol]['price'] * 100
+                logging.info(f"{symbol} (롱) MACD 반환점 도달! 수익률: {profit:.2f}%")
+                bot.sendMessage(chat_id = chat_id, text=f"{symbol} (롱) MACD 반환점 도달! 수익률: {profit:.2f}%")
 
-                elif info[symbol]['position'] == 'long':
-                    if stoch_osc_yes > 0 and stoch_osc_to < 0 and mfi > 70:
-                        order = binance.create_limit_sell_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매도 주문
-                        info[symbol]['position'] = 'wait'
-                        current_hold -= 1
-                        profit = (current_price - info[symbol]['start_price']) / info[symbol]['start_price'] * 100
-                        logging.info(f"{symbol} (롱) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
-                        bot.sendMessage(chat_id = chat_id, text=f"{symbol} (롱) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
-                    elif macd < 0:
-                        order = binance.create_limit_sell_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매도 주문
-                        info[symbol]['position'] = 'wait'
-                        current_hold -= 1
-                        profit = (current_price - info[symbol]['start_price']) / info[symbol]['start_price'] * 100
-                        logging.info(f"{symbol} (롱) MACD 반환점 도달! 수익률: {profit:.2f}%")
-                        bot.sendMessage(chat_id = chat_id, text=f"{symbol} (롱) MACD 반환점 도달! 수익률: {profit:.2f}%")
+        elif info[symbol]['position'] == 'short':
+            if stoch_osc_yes < 0 and stoch_osc_to > 0 and mfi < 30:
+                order = binance.create_limit_buy_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매수 주문
+                info[symbol]['position'] = 'wait'
+                current_hold -= 1
+                profit = (info[symbol]['price'] - current_price) / current_price * 100
+                logging.info(f"{symbol} (숏) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
+                bot.sendMessage(chat_id = chat_id, text=f"{symbol} (숏) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
+            elif macd > 0:
+                order = binance.create_limit_buy_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매수 주문
+                info[symbol]['position'] = 'wait'
+                current_hold -= 1
+                profit = (info[symbol]['price'] - current_price) / current_price * 100
+                logging.info(f"{symbol} (숏) MACD 반환점 도달! 수익률: {profit:.2f}%")
+                bot.sendMessage(chat_id = chat_id, text=f"{symbol} (숏) MACD 반환점 도달! 수익률: {profit:.2f}%")
 
-                elif info[symbol]['position'] == 'short':
-                    if stoch_osc_yes < 0 and stoch_osc_to > 0 and mfi < 30:
-                        order = binance.create_limit_buy_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매수 주문
-                        info[symbol]['position'] = 'wait'
-                        current_hold -= 1
-                        profit = (info[symbol]['start_price'] - current_price) / current_price * 100
-                        logging.info(f"{symbol} (숏) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
-                        bot.sendMessage(chat_id = chat_id, text=f"{symbol} (숏) Stochastic 반환점 도달! 수익률: {profit:.2f}%")
-                    elif macd > 0:
-                        order = binance.create_limit_buy_order(symbol, info[symbol]['quantity'], current_price) # 지정가 매수 주문
-                        info[symbol]['position'] = 'wait'
-                        current_hold -= 1
-                        profit = (info[symbol]['start_price'] - current_price) / current_price * 100
-                        logging.info(f"{symbol} (숏) MACD 반환점 도달! 수익률: {profit:.2f}%")
-                        bot.sendMessage(chat_id = chat_id, text=f"{symbol} (숏) MACD 반환점 도달! 수익률: {profit:.2f}%")
+    except Exception as e:
+        bot.sendMessage(chat_id = chat_id, text=f"에러발생 {e}")
+        logging.error(e)
+    time.sleep(0.5)
 
-            except Exception as e:
-                bot.sendMessage(chat_id = chat_id, text=f"에러발생 {e}")
-                logging.error(e)
-            time.sleep(0.5)
-
-        # 파일에 수집한 정보 및 거래 정보 파일에 저장
-        with open('./Data/binance_swing.txt', 'w') as f:
-            f.write(json.dumps(info))
+# 파일에 수집한 정보 및 거래 정보 파일에 저장
+with open('./Data/binance_swing.txt', 'w') as f:
+    f.write(json.dumps(info))
 
